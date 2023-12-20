9079dccecf96b8f8e39659b2d64aa2bbff237b3d
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2023-12-20 17:18:43 +0530

    [Omkar] Add : program validate the user email

--- user_registration.py
+++ user_registration.py
@@ -16,9 +16,10 @@ import re
 
 class UserRegistration:
 
-    def __init__(self, f_name, l_name):
+    def __init__(self, f_name, l_name, email):
         self.f_name = f_name
         self.l_name = l_name
+        self.email = email
 
     def check_first_name(self):
         pattern = re.compile(r'^[A-Z][A-Za-z]{2,}$')
@@ -32,12 +33,21 @@ class UserRegistration:
             return True
         return False
 
+    def check_email(self):
+        pattern = re.compile(r'^[a-z]+\.?[a-z0-9]*?@[a-z]+\.[a-z0-9]+\.?[a-z]+?$')
+        #                       abc     .xyz        @   bl  .co     .in
+
+        if pattern.match(self.email):
+            return True
+        return False
+
 
 if __name__ == '__main__':
     first_name = input("Enter the first name ")
     last_name = input("Enter the last name ")
+    email = input("Enter the email ")
 
-    user = UserRegistration(first_name, last_name)
+    user = UserRegistration(first_name, last_name, email)
 
     if user.check_first_name():
         print("First name is valid ")
@@ -48,4 +58,9 @@ if __name__ == '__main__':
         print("Last name is valid ")
     else:
         print("Last name is Not valid ")
+
+    if user.check_email():
+        print("Email is valid ")
+    else:
+        print("Email is Not valid")
 
