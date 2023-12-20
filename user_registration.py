a08b26b7c4ff14a8f15fd87d0121c9deee72b250
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2023-12-20 17:14:33 +0530

    [Omkar] Add : program to validate the user last name

--- user_registration.py
+++ user_registration.py
@@ -16,8 +16,9 @@ import re
 
 class UserRegistration:
 
-    def __init__(self, f_name):
+    def __init__(self, f_name, l_name):
         self.f_name = f_name
+        self.l_name = l_name
 
     def check_first_name(self):
         pattern = re.compile(r'^[A-Z][A-Za-z]{2,}$')
@@ -25,13 +26,26 @@ class UserRegistration:
             return True
         return False
 
+    def check_last_name(self):  # ^ string start with
+        pattern = re.compile(r'^[A-Z][A-Za-z]{2,}$')  # $ end of the string
+        if pattern.match(self.l_name):
+            return True
+        return False
+
 
 if __name__ == '__main__':
     first_name = input("Enter the first name ")
+    last_name = input("Enter the last name ")
 
-    user = UserRegistration(first_name)
+    user = UserRegistration(first_name, last_name)
 
     if user.check_first_name():
         print("First name is valid ")
     else:
         print("First name is Not valid ")
+
+    if user.check_last_name():
+        print("Last name is valid ")
+    else:
+        print("Last name is Not valid ")
+
