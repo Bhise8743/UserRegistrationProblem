29f8c9f67d5cce53d265dff0551b9725da2caf42
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2023-12-21 11:28:42 +0530

    [Omkar] Add : program to test the password is correct or not

--- test_user_reg.py
+++ test_user_reg.py
@@ -4,12 +4,12 @@ from user_registration import UserRegistration
 
 @pytest.fixture
 def user_obj_t():
-    return UserRegistration("Omkar", "Bhise", "omkarbhise8745@gmail.com", "91 6235236988")
+    return UserRegistration("Omkar", "Bhise", "omkarbhise8745@gmail.com", "91 6235236988", "Bhise@123")
 
 
 @pytest.fixture
 def user_obj_f():
-    return UserRegistration("omkar", "nj", "adadfa@adf", "91 5623412323")
+    return UserRegistration("omkar", "nj", "adadfa@adf", "91 5623412323", "bhise@123")
 
 
 def test_first_name_success(user_obj_t, user_obj_f):
@@ -30,3 +30,8 @@ def test_mobile_num(user_obj_t, user_obj_f):
 def test_check_email(user_obj_f, user_obj_t):
     assert user_obj_t.check_email() is True
     assert user_obj_f.check_email() is False
+
+
+def test_check_password(user_obj_t, user_obj_f):
+    assert user_obj_t.check_password() is True
+    assert user_obj_f.check_password() is False
